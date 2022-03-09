a797606158b8bbab2b0e65a4bf6a08e088d983ed
Elijah Keeswood <ekeeswood@email.arizona.edu>
2022-03-09 16:39:29 -0700

    New Start Rollers

--- StartRollers.py
+++ StartRollers.py
@@ -19,29 +19,51 @@ rollerA2 = 0
 rollerB1 = 14
 rollerB2 = 15
 
+#pin setups
 io.setwarnings(False)
+
 io.setup(rollerA1, io.OUT)
 io.setup(rollerA2, io.OUT)
+
 io.setup(roller_standby, io.OUT)
+
 io.setup(rollerA_pwm, io.OUT)
 rollerA_pwm = io.PWM(rollerA_pwm, 1000)
 
+io.setup(rollerB_pwm, io.OUT)
+rollerB_pwm = io.PWM(rollerB_pwm, 1000)
+
 io.output(roller_standby, True)
 
-def clockwise():
+
+#Rolling functions
+def inwards():
     io.output(rollerA1, True)    
     io.output(rollerA2, False)
 
-def counter_clockwise():
+    io.output(rollerB1, False)    
+    io.output(rollerB2, True)
+
+def outwards():
     io.output(rollerA1, False)
     io.output(rollerA2, True)
 
+    io.output(rollerB1, True)
+    io.output(rollerB2, False)
 
+
+#start pwm
 rollerA_pwm.start(100)
+rollerB_pwm.start(100)
 
-counter_clockwise()
-sleep(2)
+#Main Process
+inwards()
+sleep(4)
+outwards()
+sleep(4)
+
 rollerA_pwm.stop()
+rollerB_pwm.stop()
 
 
-io.output(standby_pin, False)
+io.output(roller_standby, False)
