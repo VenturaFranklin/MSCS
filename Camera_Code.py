ce73a43cc30c9077efc7ff9e041a2c41371b179a
Dayton Brown <81769418+DBrown2021@users.noreply.github.com>
2022-03-26 16:39:09 -0700

    Update Camera_Code.py

--- Camera_Code.py
+++ Camera_Code.py
@@ -2,7 +2,7 @@ import os
 import datetime as dt
 
 
-def Take_Pic(slide_num, Before_After):
+def Take_Pic(slide_num, Before_After):    #Function to take the before and after cleaning pics
   if Before_After is False:               #Check if the picture taken is the before or after cleaning picture
     pic_order = 'Before_';
   else:
@@ -14,3 +14,9 @@ def Take_Pic(slide_num, Before_After):
   st_Slide_num = '_' + str(Slide_num);    #Convert the slide number into a string
   Filename = pic_directory + date + pic_order + st_Slide_num + file_type    #Path and file name for the picture to be used in the take picture command
   os.system("libcamera-still -t 1000 -o " +Filename)  #System Command to take a picture with the camera
+
+  def Take_Test_Pic():
+    pic_directory = '/home/pi/MSCS_Pics/';  #Directory to save the pictures in
+    file_name_type = 'test.jpg'; 
+    Filename_path = pic_directory  + file_name_type
+    os.system("libcamera-still -t 1000 -o " +Filename_path)
