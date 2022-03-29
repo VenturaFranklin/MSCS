e800a26c7023a5c8336d9830721bab5a8e6a31c1
Dayton Brown <81769418+DBrown2021@users.noreply.github.com>
2022-03-29 16:41:22 -0700

    Update Camera_Code.py

--- Camera_Code.py
+++ Camera_Code.py
@@ -5,6 +5,8 @@ import datetime as dt
 def Take_Pic(slide_num, Before_After):    #Function to take the before and after cleaning pics
   if Before_After is False:               #Check if the picture taken is the before or after cleaning picture
     pic_order = 'Before_';
+    #os.system("v4l2-ctl -c focus_abslute=570")
+    #The line above sets what the camera is focused on. Currently waiting on testing to determine the numerical value.
   else:
     pic_order = 'After_';
   pic_directory = '/home/pi/MSCS_Pics/';  #Directory to save the pictures in
@@ -17,6 +19,6 @@ def Take_Pic(slide_num, Before_After):    #Function to take the before and after
 
 def Take_Test_Pic():
   pic_directory = '/home/pi/MSCS_Pics/';  #Directory to save the pictures in
-  file_name_type = 'test.jpg';    
-  Filename_path = pic_directory  + file_name_type
-  os.system("libcamera-still -t 1000 -o " +Filename_path)
+  file_name_type = 'test.jpg';            #String for the picture file name
+  Filename_path = pic_directory  + file_name_type #Path and file name for take picture commanf
+  os.system("libcamera-still -t 1000 -o " +Filename_path) #System Command to take a picture with the camera
